db9e0ebab6a8515d5d6ef697398440ae99aab0b4
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-09-30 20:24:26 +0600

    Update Prime generation using Harmonic series .cpp

--- Prime Number/Prime generation using Harmonic series .cpp
+++ Prime Number/Prime generation using Harmonic series .cpp
@@ -1,46 +1,46 @@
 //T.C: O(nln(n))
 //S.C: O(1e8)
 
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
 const int mx = 1e8;
-bitset<mx> isPrime;
+bool isPrime[mx];
 vector<int> primes;
 
 void primeGen ( int n )
 {
-    for ( int i = 3; i <= n; i += 2 ) isPrime[i] = 1;
+    for ( int i = 2; i <= n; i++ ) isPrime[i] = 1;
 
-    int sq = sqrt(n);
-    for ( int i = 3; i <= sq; i += 2 ) {
-        if(isPrime[i]) {
-            for ( int j = i*i; j <= n; j += i ) {
+
+    for ( int i = 2; i <= n; i++ ) {
+            for ( int j = i+i; j <= n; j += i ) {
                 isPrime[j] = 0;
             }
-        }
     }
 
-    primes.push_back(2);
 
-    for ( int i = 3; i <= n; i += 2 ) {
+    for ( int i = 2; i <= n; i ++ ) {
         if(isPrime[i] == 1) {
             primes.push_back(i);
         }
     }
+
 }
 
 int main() {
-    
-    
+
+
      int lim;
      cin>>lim;
 
        primeGen(lim);
-       
+
        for(int i=0;i<primes.size();i++){
            cout<<primes[i]<<" ";
        }
-       
+
 }
+
