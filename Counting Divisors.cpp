fbaa639e3210ff74db09531877edef41b33f1daf
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-09-30 17:58:04 +0600

    Create Counting Divisors.cpp

---
+++ Counting Divisors.cpp
@@ -0,0 +1,37 @@
+// CSES-Link : https://cses.fi/problemset/task/1713/
+
+//
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+const int x=1e6+123;
+int arr[x];
+ 
+int main() {
+    
+   
+    
+    for(int i=1;i<=x;i++){
+        
+        for(int j=i;j<=x;j+=i){
+            arr[j]++;
+        }
+    }
+    
+    
+    
+     int n;
+    cin>>n;
+    
+    while(n--){
+        
+        int x;
+        cin>> x;
+        
+        cout<<arr[x]<<endl;
+    }
+    
+    
+}
