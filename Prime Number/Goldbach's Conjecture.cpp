0fd2a1002f12d723d46d5aa3ed032cb2e307a548
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-10-01 14:19:38 +0600

    Create  Goldbach's Conjecture.cpp

---
+++ Prime Number/Goldbach's Conjecture.cpp
@@ -0,0 +1,67 @@
+
+// vJ link : https://vjudge.net/problem/UVA-543
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+const int mx = 1e6+124;
+bitset<mx> isPrime;
+vector<int> primes;
+
+void primeGen ( int n )
+{
+    for ( int i = 3; i <= n; i += 2 ) isPrime[i] = 1;
+
+    int sq = sqrt(n);
+    for ( int i = 3; i <= sq; i += 2 ) {
+        if(isPrime[i]) {
+            for ( int j = i*i; j <= n; j += i ) {
+                isPrime[j] = 0;
+            }
+        }
+    }
+
+    primes.push_back(2);
+
+    for ( int i = 3; i <= n; i += 2 ) {
+        if(isPrime[i] == 1) {
+            primes.push_back(i);
+        }
+    }
+}
+
+int main() {
+
+
+     int lim=1e6;
+     primeGen(lim);
+     
+     int n;
+     
+     while(cin>>n){
+         
+         if(n==0)break;
+          
+         bool flag=false;
+          
+         for(auto i:primes){
+             int a=i;
+             int b=n-i;
+             
+             if(isPrime[b]){
+                 cout<<n<<" = "<<a<<" + "<<b<<endl;
+                 flag=true;
+                 break;
+             }
+         }
+         
+         if(flag==false){
+             cout<<"Goldbach's conjecture is wrong."<<endl;
+         }
+    }
+    
+  return 0;
+
+      
+}
