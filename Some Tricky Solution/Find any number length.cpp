0e6ad93d9b9003e3298929936d222b5e612e4190
Shanta Shil <121443679+ShantoOBS@users.noreply.github.com>
2024-10-02 18:17:49 +0600

    Create Find any number length.cpp

---
+++ Some Tricky Solution/Find any number length.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int num;
+  cin>>num;
+  
+  int temp=log10(num)+1;
+  
+   cout<<temp<<endl;
+}
